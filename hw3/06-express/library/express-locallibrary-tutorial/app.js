60d2a6543f7f9966719a85a92454c19ac8019a9b
Sam Huebschwerlen <hueb@pdx.edu>
2020-11-15 17:03:36 -0800

    production work

--- hw3/06-express/library/express-locallibrary-tutorial/app.js
+++ hw3/06-express/library/express-locallibrary-tutorial/app.js
@@ -17,7 +17,16 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://hueb:centrum85@libclust.k3gyw.mongodb.net/local_library?retryWrites=true&w=majority';
+
+//var mongoDB = 'mongodb+srv://hueb:centrum85@libclust.k3gyw.mongodb.net/local_library?retryWrites=true&w=majority';
+
+// Set up mongoose connection
+//var dev_db_url = 'mongodb+srv://hueb:centrum85@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+
+var dev_db_url = 'mongodb+srv://hueb:centrum85@libclust.k3gyw.mongodb.net/local_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
